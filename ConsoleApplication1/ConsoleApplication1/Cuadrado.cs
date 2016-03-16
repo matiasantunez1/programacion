4fb8b98b0a6afb7fab92ca1147842f4a23984fa5
matiasantunez1 <matiasantunez2525@gmail.com>
2016-03-16 11:00:49 -0300

    Added files via upload

---
+++ ConsoleApplication1/ConsoleApplication1/Cuadrado.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Cuadrado : Figura
+    {
+        public Cuadrado(float Base, float altura)
+        {
+            this.Base = Base;
+            this.Altura = Altura;
+        }
+
+        public override float Area() 
+        {
+            return (Base * Base);
+        }
+        public override float Perimetro()
+        {
+            return Base * 4;
+        }
+    
+    }
+}
---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Figura f;
+            f = new Circulo(10);
+            Console.WriteLine(f.Area());
+            f = new Triangulo(8, 4);
+            Console.WriteLine(f.Area());
+            
+            f = new Cuadrado(6, 5);
+            Console.WriteLine(f.Area());
+            Console.ReadKey();
+
+           
+        }
+    }
+}
